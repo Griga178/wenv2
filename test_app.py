8b159cb65d79b72db6f4d53a5c0bf695d3dc2fc4
Griga178 <grisha.tis@gmail.com>
2021-10-15 16:35:33 +0300

    another test_site

--- test_app.py
+++ test_app.py
@@ -17,18 +17,40 @@ file_name = 'comp_info.pkl'
 with open(file_name, 'rb') as f:
     pickle_set = pickle.load(f)
 
-def inn_list(amount = 10):
+def inn_list():
     # создаем список ключей
     key_list = []
     for el in pickle_set:
         key_list.append(el)
-    return key_list[:amount]
+    return key_list
 
+print('я выполнен')
+a = 10
 # Главная страница
-@app.route('/')
+@app.route('/', methods = ('GET', 'POST'))
 def index():
-    posts = inn_list()
-    return render_template('test_index.html', posts = posts)
+    my_list = inn_list()
+    message = str(len(inn_list()))
+    #posts = inn_list()
+
+    if request.method == 'POST':
+        word = request.form['index']
+
+        if not word:
+            flash('Title is required!')
+        else:
+            a = 10
+            #a += 10
+            #posts = inn_list(a)
+            # выполнение функции
+
+            #return redirect(url_for('index'))
+
+
+
+
+
+    return render_template('test_index.html', message = message) #posts = posts,
 
 # добавление слова в множество
 @app.route('/create', methods = ('GET', 'POST'))
